c027cf84b3c8ddca43816ed181cfd316490a48ac
Taylor Hellam <taylorhellam@gmail.com>
2016-12-06 14:18:07 -0800

    Adding start of program. Mostly testing environment

--- program.py
+++ program.py
@@ -1,1 +1,25 @@
-print('JSONline Editor')
+current_json = {}
+
+
+def main():
+    print_header()
+    do_stuff()
+    print_json()
+
+
+def print_header():
+    print('|-------------------------|')
+    print('| JSON Commandline Editor |')
+    print('|-------------------------|')
+
+
+def print_json():
+    for key in current_json:
+        print(key)
+
+
+def do_stuff():
+    current_json['test'] = 'value'
+
+
+main()
